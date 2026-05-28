792a0eedd5fd06f1a3bfe9441d921599225d73e6
benjflowers <benjaminjflowers@gmail.com>
2026-05-28 00:08:41 -0500

    adding the schema

--- prisma.config.ts
+++ prisma.config.ts
@@ -8,7 +8,6 @@ export default defineConfig({
   // Database connection details
   datasource: {
     url: env('DATABASE_URL'),
-    directUrl: env('DATABASE_URL_UNPOOLED'),
   },
 
   // Migration settings
